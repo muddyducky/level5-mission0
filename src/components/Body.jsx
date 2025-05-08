b7e5f2b2ce51d3aba91ed40681c142459be5ce19
Rachel Mareraki <rjm.mareraki@gmail.com>
2025-05-09 11:44:53 +1200

    added comments to components

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,14 @@
 import React from "react";
 import styles from "./Hero.module.css";
 
+// menuOpen passed as props from app.jsx and used to check if the menu is open
 export default function Body({ menuOpen }) {
   return (
+
+    // if menu is open, apply these styles if not apply these styles instead
     <div className={menuOpen ? styles.hideContent : styles.heroContainer}>
+
+      {/* If the menu is not open display this section otherwise hide it */}
       {!menuOpen && (
         <div className={styles.background}>
           <h1>"YOUR Vision, OUR Strategy, UNSTOPPABLE Growth" </h1>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,8 @@
 import { Link, NavLink } from "react-router-dom";
 import styles from "./Navbar.module.css";
 
+// setMenuOpen and menuOpen initializes the state of the menu, passed as props from App jsx
+
 export default function Navbar({ setMenuOpen, menuOpen }) {
   return (
     <div className={styles.navContainer}>
@@ -10,12 +12,19 @@ export default function Navbar({ setMenuOpen, menuOpen }) {
           <h3> Marketing Association NZ</h3>
         </Link>
       </div>
+
+      {/* Menu is visible in mobile view. On click function toggles visibility of the menu */}
       <div className={styles.menu} onClick={() => setMenuOpen(!menuOpen)}>
         <img src="../../images/menu.png" alt="menuIcon" />
       </div>
+
+      {/* Checks to see if the menu is open, hides links when true */}
       <div className={`${styles.links} ${menuOpen ? styles.showLinks : ""}`}>
         <ul>
           <li>
+            
+            {/* NavLinks used to navigate to different components in the app */}
+            {/* isActive built in Navlink function used to highlight which component is currently active */}  
             <NavLink
               to="/about"
               className={({ isActive }) => (isActive ? styles.active : "")}
